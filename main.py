fd8b1646a2319c8e3344a6863af91e1b41f26e59
Roman Deriga <deriga.roman@gmail.com>
2023-08-15 21:52:43 +0700

    Create function for downloading images

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import requests
 from pathlib import Path
 
 
+def download_save_image(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
 def main():
     filename = 'images/hubble.jpeg'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    response = requests.get(url)
-    response.raise_for_status()
+    download_save_image(url, filename)
 
     Path('images').mkdir(parents=True, exist_ok=True)
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    
 
 if __name__ == '__main__':
     main()

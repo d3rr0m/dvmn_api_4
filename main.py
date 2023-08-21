d1acdd2fc9b91426e79e88ec25c8fa90f7a1d6d0
Roman Deriga <deriga.roman@gmail.com>
2023-08-21 16:06:37 +0300

    Staged commit

--- main.py
+++ main.py
@@ -10,6 +10,36 @@ from datetime import date, timedelta
 config = dotenv_values('.env')
 
 
+def get_nasa_epic_available_date():
+    url = 'https://api.nasa.gov/EPIC/api/natural/all'
+    params = {
+        'api_key': config['NASA_API_TOKEN'],
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()[0]
+
+
+def fetch_nasa_epic_photos(dates):
+    url = 'https://api.nasa.gov/EPIC/api/natural/date/{date}'
+    params = {
+        'api_key': config['NASA_API_TOKEN'],
+    }
+
+    for date in dates:
+        response = requests.get(
+            url=url.format(date=date['date']),
+            params=params,
+            )
+        response.raise_for_status()
+        image_name = response.json()[0]['image']
+        image_url = \
+         f'https://epic.gsfc.nasa.gov/archive/natural/2023/08/15/png/\
+            {image_name}.png'
+        download_save_image(image_url, f'images/{image_name}.png')
+
+
+
 def fetch_spacex_last_launch():
     spacex_api_url = 'https://api.spacexdata.com/v5/launches/{id}'
     spacex_flight_id = '5eb87ce3ffd86e000604b336'
@@ -40,11 +70,11 @@ def get_file_extension(url):
     return extension
 
 
-def fetch_nasa_apod_massively(count):
+def fetch_nasa_apod_bulk(count):
     image_url = 'https://api.nasa.gov/planetary/apod'
     params = {
         'api_key': config['NASA_API_TOKEN'],
-        'count': count
+        'count': count,
     }
     response = requests.get(image_url, params=params)
     response.raise_for_status()
@@ -60,7 +90,7 @@ def fetch_nasa_apod_today():
     yeasterday_date = str(date.today()-timedelta(days=1))
     params = {
         'api_key': config['NASA_API_TOKEN'],
-        'date': yeasterday_date
+        'date': yeasterday_date,
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
@@ -71,7 +101,9 @@ def fetch_nasa_apod_today():
 
 
 def main():
-    fetch_nasa_apod_massively(5)
+    #fetch_nasa_epic_photos(get_nasa_epic_available_date())
+    print(get_nasa_epic_available_date())
+    #fetch_nasa_apod_bulk(5)
     #fetch_nasa_apod_today()
     #download_save_image()
     #fetch_spacex_last_launch()
